d4551da477f6b20ef5c989ac4c863363ad8ec367
Benjamin Woodruff <bgw@fb.com>
2019-06-06 15:35:18 -0700

    Improve setup.py compatibility
    - Add `libcst/__init__.py` back which I accidentally deleted in another
      commit.
    - Add `*.egg-info/` to the gitignore, because `libcst.egg-info` is it's
      created by pip/setuptools when locally installing libcst, and it's
      annoying.
    - Changed the version number from `0.1.dev` to `0.1.dev0`, since pip was
      warning that it was normalizing the version number from the former to
      the later.
    - Add a `python_requires` field, since we know that libcst only works on
      3.6+.
    - Add an `install_requires`. Pip uses this to find dependencies, and
      ignores `requirements.txt` (since `requirements.txt` is really only
      intended to be a freeze file).
    - Add the dataclasses backport as a dependency for Python 3.6. I
      validated that installing and using libcst works in both 3.6 and 3.7.
    
    **Test Plan:**
    
    ```
    $ python3 -m venv libcst-install-test  # my system python is 3.7
    
    $ libcst-install-test/bin/pip install --upgrade pip ipython
    Cache entry deserialization failed, entry ignored
    Collecting pip
      Using cached
      https://files.pythonhosted.org/packages/5c/e0/be401c003291b56efc55aeba6a80ab790d3d4cece2778288d65323009420/pip-19.1.1-py2.py3-none-any.whl
    Collecting ipython
    ...  # lots of output
    
    $ ~/libcst-install-test/bin/pip install ~/libcst/
    Processing ./libcst
    Requirement already satisfied: parso in
    ./libcst-install-test/lib/python3.7/site-packages (from
    libcst==0.1.dev0) (0.4.0)
    Collecting typing_extensions (from libcst==0.1.dev0)
      Using cached
      https://files.pythonhosted.org/packages/0f/62/c66e553258c37c33f9939abb2dd8d2481803d860ff68e635466f12aa7efa/typing_extensions-3.7.2-py3-none-any.whl
    Installing collected packages: typing-extensions, libcst
      Running setup.py install for libcst ... done
      Successfully installed libcst-0.1.dev0 typing-extensions-3.7.2
    
    $ ~/libcst-install-test/bin/ipython
    Python 3.7.3 (default, Apr  3 2019, 05:39:12)
    Type 'copyright', 'credits' or 'license' for more information
    IPython 7.5.0 -- An enhanced Interactive Python. Type '?' for help.
    
    In [1]: from libcst import parser
    
    In [2]: parser.parse_expression("None")
    Out[2]:
    Name(
        value='None',
        lpar=[],
        rpar=[],
    )
    
    In [3]:
    ```
    
    I then repeated the same with a copy of CPython 3.6 that I built from
    source.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import sys
 from os import path
 
 # pyre-ignore Pyre doesn't know about setuptools.
@@ -13,11 +14,18 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+extra_install_requires = []
+if sys.version_info < (3, 7, 0):
+    # install the backport
+    extra_install_requires.append("dataclasses")
+
 setuptools.setup(
     name="libcst",
     description="A concrete syntax tree with AST-like properties for Python 3.7.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="0.1.dev",
+    version="0.1.dev0",
     packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=["parso", "typing_extensions", *extra_install_requires]
 )
