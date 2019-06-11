31c0ee65a2bf71a4c263b3f54c9eb525bae717cc
Benjamin Woodruff <bgw@fb.com>
2019-06-11 11:37:09 -0700

    Export maybe and removal sentinels from __init__
    Otherwise, these aren't exposed publicly anywhere.
    
    This was @DragonMinded's change, I'm just importing it to github.

--- libcst/__init__.py
+++ libcst/__init__.py
@@ -2,3 +2,9 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+
+from libcst._maybe_sentinel import MaybeSentinel
+from libcst._removal_sentinel import RemovalSentinel
+
+
+__all__ = ["MaybeSentinel", "RemovalSentinel"]
