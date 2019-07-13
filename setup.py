1a55071f472ccdb707838258fac9585b9cd22953
Jimmy Lai <yurinai@gmail.com>
2019-07-13 12:58:32 -0700

    remove unused import sys

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import sys
 from os import path
 
 # pyre-ignore Pyre doesn't know about setuptools.
