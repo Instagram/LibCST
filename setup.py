6138e7454295eb6c63c43efb1a0ac1380b0474f1
jimmylai <yurinai@gmail.com>
2019-08-06 16:34:03 -0700

    [document] add jupyter based tutorial page
    * [document] add jupyter based tutorial page
    
    * add return type and fix typos

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import setuptools
 
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
 setuptools.setup(
