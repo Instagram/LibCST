ef0ce06e128d7bb3398f394605479c82fd8c3fff
Benjamin Woodruff <bgw@fb.com>
2019-08-06 17:05:45 -0700

    Remove unused sphinx plugins
    It doesn't look like we need:
    
    - `sphinx.ext.githubpages`
    - `sphinx.ext.todo`
    - `sphinx.ext.mathjax`
    
    So I removed these and then sorted the list of extensions.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,15 +44,12 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'nbsphinx',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinx.ext.githubpages',
     'sphinx.ext.graphviz',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
     'sphinx_rtd_theme',
-    'nbsphinx',
-    'sphinx.ext.mathjax',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
