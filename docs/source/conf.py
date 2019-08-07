2d7ace9b8a80dd463c04ca4cb17aab74b244233c
Benjamin Woodruff <bgw@fb.com>
2019-08-07 12:06:56 -0700

    Add logo and favicon to sphinx documentation
    - The white logo variant shows up on the top of the sidebar.
    - The favicon shows up when the page is loaded.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -91,7 +91,7 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {"logo_only": True}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -108,6 +108,8 @@ html_static_path = ['_static']
 #
 # html_sidebars = {}
 
+html_logo = "_static/logo/horizontal_white_sidebar.png"
+html_favicon = "_static/logo/favicon.ico"
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
