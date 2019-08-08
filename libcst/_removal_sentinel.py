4f662b9b0059b7c2fdc5989052961eeed2d72943
Benjamin Woodruff <bgw@fb.com>
2019-08-07 18:05:33 -0700

    Add docs for MaybeSentinel and RemovalSentinel
    These were referred to by other parts of the documentation, and they
    provide core parts of our functionality, so we should have explicit
    documentation for them.

--- libcst/_removal_sentinel.py
+++ libcst/_removal_sentinel.py
@@ -5,8 +5,8 @@
 
 # pyre-strict
 """
-Used and re-exported by visitor. This is hoisted into a separate module to avoid some
-circular dependencies in the definition of CSTNode.
+Used by visitors. This is hoisted into a separate module to avoid some circular
+dependencies in the definition of CSTNode.
 """
 
 from enum import Enum, auto
@@ -14,23 +14,22 @@ from enum import Enum, auto
 
 class RemovalSentinel(Enum):
     """
-    A RemovalSentinel value should be returned by a `on_leave` method when we want to
-    remove that child from its parent.
+    A :attr:`RemovalSentinel.REMOVE` value should be returned by a
+    :meth:`CSTTransformer.on_leave` method when we want to remove that child from its
+    parent.
 
-    The parent's _visit_and_replace_children method should make a best-effort to remove
-    the child, but may raise an exception when removing the child doesn't make sense, or
-    could change the semantics in an unexpected way. E.g. a function with no name
-    doesn't make sense.
+    The parent node should make a best-effort to remove the child, but may raise an
+    exception when removing the child doesn't make sense, or could change the semantics
+    in an unexpected way. E.g. a function with no name doesn't make sense.
 
     In we can't automatically remove the child, the developer should instead remove the
-    child by constructing a new parent in the parent's on_leave call.
+    child by constructing a new parent in the parent's :meth:`~CSTTransformer.on_leave`
+    call.
 
-    We use this instead of `None` to force developers to be more explicit about
-    deletions, because `None` is the default return value for a function with no return
-    statement.
-
-    In the future, we may extend this to support other forms of removal, but that's
-    unlikely.
+    We use this instead of ``None`` to force developers to be explicit about deletions.
+    Because ``None`` is the default return value for a function with no return
+    statement, it would be too easy to accidentally delete nodes from the tree by
+    forgetting to return a value.
     """
 
     REMOVE = auto()
