bc755df5605530b5d36f1e9d6ff1ab26c4e86cbf
Ray Zeng <rayzeng@fb.com>
2019-08-09 16:05:48 -0700

    Update docstrings to reflect changes in API

--- libcst/metadata/position_provider.py
+++ libcst/metadata/position_provider.py
@@ -16,19 +16,10 @@ PositionProvider = Union["BasicPositionProvider", "SyntacticPositionProvider"]
 
 class BasicPositionProvider(BaseMetadataProvider[CodeRange]):
     """
-    Generates basic line and column metadata. Basic position is
-    defined by the start and ending bounds of a node including all whitespace
+    Generates basic line and column metadata. Basic position is defined by the
+    start and ending bounds of a node including all whitespace
     owned by that node.
     """
-
-    # TODO: remove this
-    def _run(self, module: _ModuleT) -> _ModuleT:
-        """
-        Override default generate behavior as position information is
-        calculated through codegen instead of a standard visitor.
-        """
-        module.code_for_node(module, provider=self)
-        return module
 
     def _gen_impl(self, module: _ModuleT) -> None:
         module.code_for_node(module, provider=self)
@@ -36,7 +27,7 @@ class BasicPositionProvider(BaseMetadataProvider[CodeRange]):
 
 class SyntacticPositionProvider(BasicPositionProvider):
     """
-    Generates Syntactic line and column metadata. Syntactic position is
-    defined by the start and ending bounds of a node ignoring most instances
-    of leading and trailing whitespace when it is not syntactically significant.
+    Generates Syntactic line and column metadata. Syntactic position is defined
+    by the start and ending bounds of a node ignoring most instances of leading
+    and trailing whitespace when it is not syntactically significant.
     """
