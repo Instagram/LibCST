70d6ca6513cdaa7b264fd89fc185a44cbdf59671
jimmylai <yurinai@gmail.com>
2019-08-28 13:41:31 -0700

    [doc] improve metadata docs. (#50)

--- libcst/metadata/expression_context_provider.py
+++ libcst/metadata/expression_context_provider.py
@@ -13,6 +13,9 @@ from libcst.metadata.base_provider import BatchableMetadataProvider
 
 
 class ExpressionContext(Enum):
+    """Used in :class:`ExpressionContextProvider` to represent context of a variable
+    reference. """
+
     #: Load the value of a variable reference.
     #:
     #: >>> libcst.MetadataWrapper(libcst.parse_module("a")).resolve(libcst.ExpressionContextProvider)
