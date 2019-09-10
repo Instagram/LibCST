1bd5d3738f27c1e69397cc0f07d8fae7c9810e65
Jennifer Taylor <dragonminded@dragonminded.com>
2019-09-10 00:10:38 +0000

    Fork Parso's utils.py
    Brings a copy of utils.py internal to LibCST, rewriting our uses of it
    to use our internal fork. A compatibility shim (__iter__) is currently
    included to make PythonVersionInfo compatible with Parso directly.

--- libcst/_parser/_wrapped_tokenize.py
+++ libcst/_parser/_wrapped_tokenize.py
@@ -30,10 +30,10 @@ from parso.python.tokenize import (
     Token as OrigToken,
     tokenize_lines as orig_tokenize_lines,
 )
-from parso.utils import PythonVersionInfo, split_lines
 
 from libcst._add_slots import add_slots
 from libcst._exceptions import ParserSyntaxError
+from libcst._parser._parso._utils import PythonVersionInfo, split_lines
 from libcst._parser._types.token import Token
 from libcst._parser._types.whitespace_state import WhitespaceState
 
@@ -90,6 +90,7 @@ def tokenize_lines(
     lines: Sequence[str], version_info: PythonVersionInfo
 ) -> Generator[Token, None, None]:
     state = _TokenizeState(lines)
+    # pyre-ignore Temporarily ignoring version_info problem until tokenize is brought into LibCST.
     orig_tokens_iter = iter(orig_tokenize_lines(lines, version_info))
 
     # Iterate over the tokens and pass them to _convert_token, providing a one-token
