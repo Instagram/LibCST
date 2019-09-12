95fabe490e24e1ff241878fcfbec397378d6be0c
Jennifer Taylor <jetaylor@fb.com>
2019-09-12 11:38:10 -0700

    Add copyright headers to all files.

--- libcst/_parser/_parso/tests/test_fstring.py
+++ libcst/_parser/_parso/tests/test_fstring.py
@@ -1,3 +1,11 @@
+# Copyright 2004-2005 Elemental Security, Inc. All Rights Reserved.
+# Licensed to PSF under a Contributor Agreement.
+#
+# Modifications:
+# Copyright David Halter and Contributors
+# Modifications are dual-licensed: MIT and PSF.
+# 99% of the code is different from pgen2, now.
+#
 # A fork of Parso's tokenize test
 # https://github.com/davidhalter/parso/blob/master/test/test_tokenize.py
 #
