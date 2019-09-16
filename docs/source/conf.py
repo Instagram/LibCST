afd3cf6bd4f55fa3ac95138a85a5f5826db74ebd
Jimmy Lai <yurinai@gmail.com>
2019-09-16 14:42:03 -0700

    update author in Sphinx conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -137,15 +137,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (
-        master_doc,
-        "LibCST.tex",
-        "LibCST Documentation",
-        "Benjamin Woodruff, Jennifer Taylor",
-        "manual",
-    )
-]
+latex_documents = [(master_doc, "LibCST.tex", "LibCST Documentation", author, "manual")]
 
 
 # -- Options for manual page output ------------------------------------------
