ed1bc89cd42bf7bb9255b389cff7d431a72cbf4e
Jennifer Taylor <jetaylor@fb.com>
2019-11-13 15:40:42 -0800

    Bump LibCST to 0.2.4.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     description="A concrete syntax tree with AST-like properties for Python 3.5, 3.6 and 3.7 programs.",
     long_description=long_description,
     long_description_content_type="text/x-rst",
-    version="0.2.3",
+    version="0.2.4",
     url="https://github.com/Instagram/LibCST",
     license="MIT",
     packages=setuptools.find_packages(),
