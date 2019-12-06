223bcef50b242b8ff1a6c90b9761c5016216cd6f
Jennifer Taylor <jetaylor@fb.com>
2019-12-05 16:48:13 -0800

    Initial work to open-source codemod runner frontend, integrating it into libcst.tool.

---
+++ libcst/codemod/commands/noop.py
@@ -0,0 +1,8 @@
+from libcst import Module
+from libcst.codemod import CodemodCommand
+
+
+class NOOPCommand(CodemodCommand):
+    def _transform_module_impl(self, tree: Module) -> Module:
+        # Return the tree as-is, with absolutely no modification
+        return tree
