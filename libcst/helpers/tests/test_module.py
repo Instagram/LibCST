f7e13df680e2f34511e73ba49b101ff6d454c0ca
Jennifer Taylor <jetaylor@fb.com>
2019-12-12 15:34:13 -0800

    Apply correct copyright header to all open-sourced files.

--- libcst/helpers/tests/test_module.py
+++ libcst/helpers/tests/test_module.py
@@ -1,3 +1,9 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
+# pyre-strict
 import libcst
 from libcst.helpers.module import insert_header_comments
 from libcst.testing.utils import UnitTest
