e48aa6eaf7ebc1750fd75850923c74c67353b081
Jennifer Taylor <jetaylor@fb.com>
2020-01-22 11:23:37 -0800

    Implement basic templated statement/expression/module parsing.
    This has some corner cases that we do not currently handle, but those will come in a later commit.

--- libcst/helpers/__init__.py
+++ libcst/helpers/__init__.py
@@ -5,9 +5,21 @@
 #
 # pyre-strict
 
+from libcst.helpers._template import (
+    parse_template_expression,
+    parse_template_module,
+    parse_template_statement,
+)
 from libcst.helpers.common import ensure_type
 from libcst.helpers.expression import get_full_name_for_node
 from libcst.helpers.module import insert_header_comments
 
 
-__all__ = ["get_full_name_for_node", "ensure_type", "insert_header_comments"]
+__all__ = [
+    "get_full_name_for_node",
+    "ensure_type",
+    "insert_header_comments",
+    "parse_template_module",
+    "parse_template_statement",
+    "parse_template_expression",
+]
