e4137c000cfbc53f12e49d0a5d710a18b9df0d84
Zsolt Dollenstein <zsol.zsol@gmail.com>
2020-03-20 11:06:15 +0000

    add docstring and expand DESCRIPTION

--- libcst/codemod/commands/remove_unused_imports.py
+++ libcst/codemod/commands/remove_unused_imports.py
@@ -11,7 +11,23 @@ from libcst.codemod.visitors import RemoveImportsVisitor
 
 
 class RemoveUnusedImportsCommand(VisitorBasedCodemodCommand):
-    DESCRIPTION: str = "Remove all imports that are not used in a file."
+    """
+    Remove all unused imports from a file based on scope analysis.
+
+    This command analyses individual files in isolation and does not attempt
+    to track cross-references between them. If a symbol is imported in a file
+    but otherwise unused in it, that import will be removed even if it is being
+    referenced from another file.
+
+    It currently doesn't keep track of string type annotations, so an import
+    for `MyType` used only in `def f() -> "MyType"` will be removed.
+    """
+
+    DESCRIPTION: str = (
+        "Remove all imports that are not used in a file. "
+        "Note: only considers the file in isolation. "
+        "Note: does not account for usages in string type annotations. "
+    )
 
     def visit_Import(self, node: Import) -> bool:
         RemoveImportsVisitor.remove_unused_import_by_node(self.context, node)
