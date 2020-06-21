b30a94badb0e4d018f3c2a33f2fb5ebd20841f08
Josie Eshkenazi <eshkenazi.josephine@gmail.com>
2020-06-21 19:20:01 -0400

    Refactored rename codemod to take  and  qualified names as params.

--- libcst/codemod/commands/rename.py
+++ libcst/codemod/commands/rename.py
@@ -5,13 +5,13 @@
 #
 # pyre-strict
 import argparse
-from typing import Optional, Sequence, Union
+from typing import Dict, Optional, Sequence, Union
 
 import libcst as cst
 from libcst.codemod import CodemodContext, VisitorBasedCodemodCommand
 from libcst.codemod.visitors import AddImportsVisitor, RemoveImportsVisitor
 from libcst.helpers import get_full_name_for_node
-from libcst.metadata import QualifiedName, QualifiedNameProvider, QualifiedNameSource
+from libcst.metadata import QualifiedNameProvider
 
 
 class RenameCommand(VisitorBasedCodemodCommand):
@@ -26,145 +26,167 @@ class RenameCommand(VisitorBasedCodemodCommand):
     @staticmethod
     def add_args(parser: argparse.ArgumentParser) -> None:
         parser.add_argument(
-            "--orig_module",
-            dest="orig_module",
-            help="Name of the module to rename. Leave blank if renaming a local variable.",
-        )
-        parser.add_argument(
-            "--orig_object",
-            dest="orig_object",
+            "--old_name",
+            dest="old_name",
             required=True,
-            help="Name of the object in module or variable to rename.",
-        )
-        parser.add_argument(
-            "--new_module",
-            dest="new_module",
-            help="New name of the module. Leave blank if renaming a local variable, or if same as orig_module.",
-        )
-        parser.add_argument(
-            "--new_object",
-            dest="new_object",
-            required=True,
-            help="New name of the object in module or variable.",
+            help="Full dotted name of object to rename. Eg: `foo.bar.baz`",
         )
 
-    def __init__(
-        self,
-        context: CodemodContext,
-        orig_object: str,
-        new_object: str,
-        orig_module: Optional[str] = None,
-        new_module: Optional[str] = None,
-    ) -> None:
+        parser.add_argument(
+            "--new_name",
+            dest="new_name",
+            required=True,
+            help="Full dotted name of replacement object. Eg: `foo.bar.baz`",
+        )
+
+    def __init__(self, context: CodemodContext, old_name: str, new_name: str) -> None:
         super().__init__(context)
+        self.old_name = old_name
+        self.new_name = new_name
 
-        self.orig_module: Optional[str] = orig_module
-        self.orig_object: str = orig_object
-        self.orig_name: str = (
-            f"{orig_module}.{orig_object}" if orig_module is not None else orig_object
-        )
+        old_names = self.old_name.rpartition(".")
+        new_names = self.new_name.rpartition(".")
 
-        self.new_module: Optional[
-            str
-        ] = new_module if new_module is not None else orig_module
-        self.new_object: str = new_object
-        self.new_name: str = (
-            f"{new_module}.{new_object}" if new_module is not None else new_object
-        )
+        # Mapping for easy lookup later.
+        self.equivalents: Dict[str, str] = {
+            old_names[2]: new_names[2],
+            "".join(old_names[:2]): new_names[0],
+        }
 
-        self.source: QualifiedNameSource = (
-            QualifiedNameSource.IMPORT
-            if orig_module is not None
-            else QualifiedNameSource.LOCAL
-        )
+        self.scheduled_imports = False
 
-    def leave_Import(
-        self, original_node: cst.Import, updated_node: cst.Import
-    ) -> Union[cst.Import, cst.RemovalSentinel]:
-        for import_alias in original_node.names:
-            name = get_full_name_for_node(import_alias.name)
-            if name is not None and name == self.orig_module:
-                # Schedule this import to be potentially removed
-                RemoveImportsVisitor.remove_unused_import_by_node(
-                    self.context, original_node
-                )
-        return original_node
+    def visit_Import(self, node: cst.Import) -> None:
+        for import_alias in node.names:
+            alias_name = get_full_name_for_node(import_alias.name)
+            if alias_name is not None:
+                self.record_asname(import_alias, alias_name)
+
+                # If the import statement is exactly equivalent to the old name, replace it.
+                if alias_name == self.old_name:
+                    self.cleanup_imports(node=node, new_module=self.new_name)
 
     def leave_ImportFrom(
         self, original_node: cst.ImportFrom, updated_node: cst.ImportFrom
-    ) -> Union[cst.ImportFrom, cst.RemovalSentinel]:
-        names = original_node.names
+    ) -> cst.ImportFrom:
+        module = updated_node.module
+        if module is not None:
+            imported_module_name = get_full_name_for_node(module)
+            names = original_node.names
 
-        import_module = original_node.module
-        import_module_name = (
-            get_full_name_for_node(import_module) if import_module is not None else None
-        )
-        if (
-            import_module_name is None
-            or import_module_name != self.orig_module
-            or not isinstance(names, Sequence)
-        ):
-            return updated_node
-
-        if (
-            len(names) == 1
-            and get_full_name_for_node(names[0].name) == self.orig_object
-        ):
-            # Remove this ImportFrom altogether
-            return cst.RemoveFromParent()
-
-        new_names = []
-        for import_alias in names:
-            qualname = (
-                f"{import_module_name}.{get_full_name_for_node(import_alias.name)}"
-            )
-            if qualname != self.orig_name:
-                new_names.append(import_alias)
-
-        return updated_node.with_changes(names=new_names)
-
-    def leave_Attribute(
-        self, original_node: cst.Attribute, updated_node: cst.Attribute
-    ) -> cst.Attribute:
-        if QualifiedNameProvider.has_name(
-            self, original_node, QualifiedName(name=self.orig_name, source=self.source),
-        ):
-            full_name = get_full_name_for_node(original_node)
-            if full_name is None:
-                raise Exception("Logic error!")
-            module_name = ".".join(full_name.split(".")[:-1])
-            # New assignment for pyre check.
-            new_import = self.new_module
-            if new_import is not None:
-                AddImportsVisitor.add_needed_import(
-                    self.context, module=new_import, asname=None,
-                )
-            # New assignment for pyre check.
-            new_value = self.new_module if self.new_module is not None else module_name
-            # If we reach here, new_value should never technically be None since new_module and module_asname cannot both be None.
-            if new_value is not None:
+            if imported_module_name is None or not isinstance(names, Sequence):
+                return updated_node
+            elif imported_module_name == self.old_name:
+                # Simply rename the imported module on the spot.
                 return updated_node.with_changes(
-                    value=cst.parse_expression(new_value),
-                    attr=cst.Name(value=self.new_object.split(".")[-1]),
+                    module=cst.parse_expression(self.new_name)
                 )
+            else:
+                new_names = []
+                for import_alias in names:
+                    alias_name = get_full_name_for_node(import_alias.name)
+                    if alias_name is not None:
+                        qual_name = f"{imported_module_name}.{alias_name}"
+                        if self.old_name == qual_name:
+                            self.record_asname(import_alias, alias_name)
 
+                            replacement_module = self.gen_replacement(
+                                imported_module_name + "."
+                            )
+                            replacement_obj = self.gen_replacement(alias_name)
+                            new_import_alias_name: cst.BaseExpression = cst.parse_expression(
+                                replacement_obj
+                            )
+                            if not isinstance(
+                                new_import_alias_name, (cst.Attribute, cst.Name)
+                            ):
+                                raise Exception("Something went wrong!")
+                            # Rename on the spot only if this is the only imported name under the module.
+                            if len(names) == 1:
+                                return updated_node.with_changes(
+                                    module=cst.parse_expression(replacement_module),
+                                    names=(
+                                        cst.ImportAlias(name=new_import_alias_name),
+                                    ),
+                                )
+                            # Or if the module name is to stay the same.
+                            elif replacement_module == imported_module_name:
+                                new_names.append(
+                                    cst.ImportAlias(name=new_import_alias_name)
+                                )
+                            else:
+                                # Otherwise, discard the import and schedule a new import.
+                                self.cleanup_imports(
+                                    new_module=replacement_module,
+                                    new_obj=replacement_obj,
+                                )
+                        else:
+                            new_names.append(import_alias)
+
+                return updated_node.with_changes(names=new_names)
         return updated_node
 
     def leave_Name(self, original_node: cst.Name, updated_node: cst.Name) -> cst.Name:
-        if QualifiedNameProvider.has_name(
-            self, original_node, QualifiedName(name=self.orig_name, source=self.source),
-        ):
-            name = get_full_name_for_node(original_node)
-            if name is None:
-                raise Exception("Logic error!")
-
-            # New assignment for pyre check.
-            new_import = self.new_module
-            if new_import is not None:
-                AddImportsVisitor.add_needed_import(
-                    self.context, module=new_import, obj=self.new_object, asname=None
-                )
-
-            return updated_node.with_changes(value=self.new_object)
+        if QualifiedNameProvider.has_name(self, original_node, self.old_name):
+            replacement_name: str = self.gen_replacement(original_node.value)
+            return updated_node.with_changes(value=replacement_name)
 
         return updated_node
+
+    def leave_Attribute(
+        self, original_node: cst.Attribute, updated_node: cst.Attribute
+    ) -> Union[cst.Name, cst.Attribute]:
+        if (
+            QualifiedNameProvider.has_name(self, original_node, self.old_name,)
+            or get_full_name_for_node(original_node) == self.old_name
+        ):
+            new_value, _, new_attr = self.new_name.rpartition(".")
+
+            self.cleanup_imports(node=original_node.value, new_module=new_value)
+            return updated_node.with_changes(
+                value=cst.parse_expression(new_value), attr=cst.Name(value=new_attr)
+            )
+
+        return updated_node
+
+    def cleanup_imports(
+        self,
+        node: Optional[cst.CSTNode] = None,
+        new_module: Optional[str] = None,
+        new_obj: Optional[str] = None,
+    ) -> None:
+        if not self.scheduled_imports and any([node, new_module]):
+            if node is not None:
+                RemoveImportsVisitor.remove_unused_import_by_node(self.context, node)
+            if new_module is not None and new_obj is not None:
+                AddImportsVisitor.add_needed_import(
+                    self.context, module=new_module, obj=new_obj
+                )
+            elif new_module is not None:
+                AddImportsVisitor.add_needed_import(self.context, module=new_module)
+            self.scheduled_imports = True
+
+    def gen_replacement(self, original_name: str) -> str:
+        index = original_name.rfind(".")
+        parts = (
+            (original_name[: index + 1], original_name[index + 1 :])
+            if index > -1
+            else (original_name,)
+        )
+        replacement_parts = []
+
+        for part in parts:
+            if part in self.equivalents:
+                replacement_parts.append(self.equivalents[part])
+                replacement_parts.append(".")
+            elif part + "." in self.equivalents:
+                replacement_parts.append(self.equivalents[part + "."])
+                replacement_parts.append(".")
+
+        return "".join(replacement_parts).rstrip(".")
+
+    def record_asname(self, import_alias: cst.ImportAlias, alias_name: str) -> None:
+        # Record the import's `as` name if it has one, and map it to the equivalent new name.
+        as_name = import_alias.asname
+        name = as_name.name if as_name is not None else None
+        if name is not None and isinstance(name, cst.Name):
+            self.equivalents[name.value + "."] = self.gen_replacement(alias_name)
--- libcst/codemod/commands/tests/test_rename.py
+++ libcst/codemod/commands/tests/test_rename.py
@@ -30,14 +30,7 @@ class TestRenameCommand(CodemodTest):
                 qux(5)
         """
 
-        self.assertCodemod(
-            before,
-            after,
-            orig_module="foo",
-            orig_object="bar",
-            new_module="baz",
-            new_object="qux",
-        )
+        self.assertCodemod(before, after, old_name="foo.bar", new_name="baz.qux")
 
     def test_rename_name_asname(self) -> None:
 
@@ -55,12 +48,7 @@ class TestRenameCommand(CodemodTest):
         """
 
         self.assertCodemod(
-            before,
-            after,
-            orig_module="foo",
-            orig_object="bar",
-            new_module="baz",
-            new_object="qux",
+            before, after, old_name="foo.bar", new_name="baz.qux",
         )
 
     def test_rename_attr(self) -> None:
@@ -72,6 +60,7 @@ class TestRenameCommand(CodemodTest):
                 a.b.c(5)
         """
         after = """
+            import a.b
             import d.e
 
             def test() -> None:
@@ -79,12 +68,7 @@ class TestRenameCommand(CodemodTest):
         """
 
         self.assertCodemod(
-            before,
-            after,
-            orig_module="a.b",
-            orig_object="c",
-            new_module="d.e",
-            new_object="f",
+            before, after, old_name="a.b.c", new_name="d.e.f",
         )
 
     def test_rename_attr_asname(self) -> None:
@@ -103,35 +87,77 @@ class TestRenameCommand(CodemodTest):
         """
 
         self.assertCodemod(
-            before,
-            after,
-            orig_module="foo",
-            orig_object="qux",
-            new_module="baz",
-            new_object="quux",
+            before, after, old_name="foo.qux", new_name="baz.quux",
         )
 
-    def test_rename_module(self) -> None:
+    def test_rename_module_import(self) -> None:
         before = """
-            from A import B
+            import a.b
 
-            class Foo(B.some_class):
+            class Foo(a.b.C):
                 pass
         """
         after = """
-            from C import B
+            import c.b
 
-            class Foo(B.some_class):
+            class Foo(c.b.C):
                 pass
         """
 
         self.assertCodemod(
-            before,
-            after,
-            orig_module="A",
-            orig_object="B",
-            new_module="C",
-            new_object="B",
+            before, after, old_name="a.b", new_name="c.b",
+        )
+
+    def test_rename_module_import_from(self) -> None:
+        before = """
+            from a import b
+
+            class Foo(b.C):
+                pass
+        """
+        after = """
+            from c import b
+
+            class Foo(b.C):
+                pass
+        """
+
+        self.assertCodemod(
+            before, after, old_name="a.b", new_name="c.b",
+        )
+
+    def test_rename_class(self) -> None:
+        before = """
+            from a.b import some_class
+
+            class Foo(some_class):
+                pass
+        """
+        after = """
+            from c.b import some_class
+
+            class Foo(some_class):
+                pass
+        """
+        self.assertCodemod(
+            before, after, old_name="a.b.some_class", new_name="c.b.some_class",
+        )
+
+    def test_rename_object_same_module(self) -> None:
+        before = """
+            from a.b import Class_1, Class_2
+
+            class Foo(Class_1):
+                pass
+        """
+        after = """
+            from a.b import Class_3, Class_2
+
+            class Foo(Class_3):
+                pass
+        """
+        self.assertCodemod(
+            before, after, old_name="a.b.Class_1", new_name="a.b.Class_3",
         )
 
     def test_rename_local_variable(self) -> None:
@@ -145,7 +171,7 @@ class TestRenameCommand(CodemodTest):
         """
 
         self.assertCodemod(
-            before, after, orig_object="x", new_object="z",
+            before, after, old_name="x", new_name="z",
         )
 
     def test_module_does_not_change(self) -> None:
@@ -161,9 +187,7 @@ class TestRenameCommand(CodemodTest):
             class Foo(c):
                 pass
         """
-        self.assertCodemod(
-            before, after, orig_module="a", orig_object="b", new_object="c"
-        )
+        self.assertCodemod(before, after, old_name="a.b", new_name="a.c")
 
     def test_other_imports_untouched(self) -> None:
         before = """
@@ -182,12 +206,7 @@ class TestRenameCommand(CodemodTest):
                 baz: c.baz
         """
         self.assertCodemod(
-            before,
-            after,
-            orig_module="a",
-            orig_object="z",
-            new_module="d",
-            new_object="z",
+            before, after, old_name="a.z", new_name="d.z",
         )
 
     def test_other_import_froms_untouched(self) -> None:
@@ -207,12 +226,7 @@ class TestRenameCommand(CodemodTest):
                 baz: d.baz
         """
         self.assertCodemod(
-            before,
-            after,
-            orig_module="a",
-            orig_object="b",
-            new_module="f",
-            new_object="b",
+            before, after, old_name="a.b", new_name="f.b",
         )
 
     def test_no_removal_of_import_in_use(self) -> None:
@@ -234,58 +248,25 @@ class TestRenameCommand(CodemodTest):
                 pass
         """
         self.assertCodemod(
-            before,
-            after,
-            orig_module="a",
-            orig_object="b",
-            new_module="z",
-            new_object="b",
+            before, after, old_name="a.b", new_name="z.b",
         )
 
     def test_no_removal_of_import_from_in_use(self) -> None:
         before = """
-            from a import m
+            from a import b
 
-            class Foo(m.CstNode):
-                bar: m.Attribute
+            class Foo(b.some_class):
+                bar: b.some_other_class
         """
         after = """
-            from a import m
+            from a import b
             import blah
 
-            class Foo(blah.CstNode):
-                bar: m.Attribute
+            class Foo(blah.some_class):
+                bar: b.some_other_class
         """
         self.assertCodemod(
-            before,
-            after,
-            orig_module="a",
-            orig_object="m.CstNode",
-            new_module="blah",
-            new_object="CstNode",
-        )
-
-    def test_no_removal_of_import_from_in_use_2(self) -> None:
-        before = """
-            from a import m
-
-            class Foo(m.CstNode):
-                bar: m.Attribute
-        """
-        after = """
-            from a import m
-            import blah
-
-            class Foo(blah.CstNode):
-                bar: m.Attribute
-        """
-        self.assertCodemod(
-            before,
-            after,
-            orig_module="a.m",
-            orig_object="CstNode",
-            new_module="blah",
-            new_object="CstNode",
+            before, after, old_name="a.b.some_class", new_name="blah.some_class",
         )
 
     def test_other_unused_imports_untouched(self) -> None:
@@ -304,10 +285,101 @@ class TestRenameCommand(CodemodTest):
                 pass
         """
         self.assertCodemod(
-            before,
-            after,
-            orig_module="a",
-            orig_object="obj",
-            new_module="c",
-            new_object="obj",
+            before, after, old_name="a.obj", new_name="c.obj",
+        )
+
+    def test_complex_module_rename(self) -> None:
+        before = """
+            from a.b.c import d
+
+            class Foo(d.e.f):
+                pass
+        """
+        after = """
+            import g.h.i
+
+            class Foo(g.h.i.j):
+                pass
+        """
+        self.assertCodemod(before, after, old_name="a.b.c.d.e.f", new_name="g.h.i.j")
+
+    def test_names_with_repeated_substrings(self) -> None:
+        before = """
+            from aa import aaaa
+
+            class Foo(aaaa.Bar):
+                pass
+        """
+        after = """
+            from b import c
+
+            class Foo(c.Bar):
+                pass
+        """
+        self.assertCodemod(
+            before, after, old_name="aa.aaaa", new_name="b.c",
+        )
+
+    def test_import_star_attr(self) -> None:
+        before = """
+            from foo import *
+
+            def baz():
+                foo.bar(5)
+        """
+        after = """
+            from foo import *
+            import qux
+
+            def baz():
+                qux.bar(5)
+        """
+        self.assertCodemod(
+            before, after, old_name="foo.bar", new_name="qux.bar",
+        )
+
+    def test_import_star_obj(self) -> None:
+        before = """
+            from foo import *
+
+            class Bar(foo.Bar):
+                pass
+        """
+        after = """
+            from foo import *
+            import qux
+
+            class Bar(qux.Bar):
+                pass
+        """
+        self.assertCodemod(
+            before, after, old_name="foo.Bar", new_name="qux.Bar",
+        )
+
+    def test_repeated_name(self) -> None:
+        before = """
+            from foo import foo
+
+            def bar():
+                foo(5)
+        """
+        after = """
+            from qux import qux
+
+            def bar():
+                qux(5)
+        """
+        self.assertCodemod(
+            before, after, old_name="foo.foo", new_name="qux.qux",
+        )
+
+    def test_no_codemod(self) -> None:
+        before = """
+            from foo import bar
+
+            def baz():
+                bar(5)
+        """
+        self.assertCodemod(
+            before, before, old_name="bar", new_name="qux",
         )
