44e785a9c8a98a57c40531029dc754cc65a6a8e0
Zsolt Dollenstein <zsol.zsol@gmail.com>
2021-06-25 13:15:33 +0100

    add license headers

--- libcst_native/src/parser/codegen.rs
+++ libcst_native/src/parser/codegen.rs
@@ -1,3 +1,8 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 #[derive(Debug)]
 pub struct CodegenState {
     pub tokens: Vec<String>,
--- libcst_native/src/parser/whitespace.rs
+++ libcst_native/src/parser/whitespace.rs
@@ -1,3 +1,8 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 use crate::whitespace_state::WhitespaceState as State;
 use once_cell::sync::Lazy;
 use regex::Regex;
