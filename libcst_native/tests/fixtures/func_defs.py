0c040dc3bdc387e7f091a02094a149726c697d21
Zsolt Dollenstein <zsol.zsol@gmail.com>
2021-07-04 15:28:14 +0100

    add func def fixture

---
+++ libcst_native/tests/fixtures/func_defs.py
@@ -0,0 +1,10 @@
+def f(a, /,): pass
+def f(a, /, c, d, e): pass
+def f(a, /, c, *, d, e): pass
+def f(a, /, c, *, d, e, **kwargs): pass
+def f(a=1, /,): pass
+def f(a=1, /, b=2, c=4): pass
+def f(a=1, /, b=2, *, c=4): pass
+def f(a=1, /, b=2, *, c): pass
+def f(a=1, /, b=2, *, c=4, **kwargs): pass
+def f(a=1, /, b=2, *, c, **kwargs): pass
