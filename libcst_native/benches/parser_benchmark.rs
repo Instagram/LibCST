07812f18a729cf4130a23160c10e09e180e16db2
Zsolt Dollenstein <zsol.zsol@gmail.com>
2021-07-06 12:00:44 +0100

    tweak benchmarks

--- libcst_native/benches/parser_benchmark.rs
+++ libcst_native/benches/parser_benchmark.rs
@@ -1,11 +1,45 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use libcst_native::parser::parse_module;
+use std::time::Duration;
 
-pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("decorated-function", |b| {
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use indoc::indoc;
+use libcst_native::parser::{parse_module, Codegen};
+
+pub fn parser_benchmarks(c: &mut Criterion) {
+    let mut group = c.benchmark_group("parser");
+    group.measurement_time(Duration::from_secs(15));
+    group.bench_function("decorated-function", |b| {
         b.iter(|| parse_module(black_box("@hello\ndef foo(bar): ...")))
+    });
+    group.finish();
+}
+
+pub fn codegen_benchmarks(c: &mut Criterion) {
+    let m = parse_module(indoc! {r"
+        
+        @hello
+
+        def foo(
+            a, #foo
+            b=1,
+            /,
+            c=3,
+            *,
+            d, # bar
+            **kwargs,
+        ):
+            pass
+        
+        
+        def bar(): ...
+    "})
+    .expect("parse failed");
+    c.bench_function("roundtrip", |b| {
+        b.iter(|| {
+            let mut state = Default::default();
+            m.codegen(&mut state);
+        })
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, parser_benchmarks, codegen_benchmarks);
 criterion_main!(benches);
