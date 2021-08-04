0d23fa7575e20e6fb0d4fdc6fda68d83ff228026
Zsolt Dollenstein <zsol.zsol@gmail.com>
2021-08-04 11:51:26 +0100

    add string fixtures

---
+++ native/libcst/tests/fixtures/super_strings.py
@@ -0,0 +1,18 @@
+_ = ""
+_ = ''
+_ = """"""
+_ = ''''''
+
+_ = 'a'   "string" 'that'   r"is" 'concatenated ' 
+
+b"string "
+"and non f" r'string'
+
+(
+    "parenthesized"
+    "concatenated"
+    """triple
+    quoted
+    """
+    
+)
