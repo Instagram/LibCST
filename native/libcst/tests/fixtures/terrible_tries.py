1293ea7ef3f5e37777fc261601c54c7b169bb145
Zsolt Dollenstein <zsol.zsol@gmail.com>
2021-08-05 17:29:51 +0100

    add fixtures for try

---
+++ native/libcst/tests/fixtures/terrible_tries.py
@@ -0,0 +1,43 @@
+#foo.
+
+try  :
+    bar()
+
+finally  :
+    pass
+
+
+try   :
+    pass
+
+except :
+
+    pass
+
+except  lol  as  LOL  :
+
+    pass
+
+else  :
+
+    pass
+
+finally  :
+
+    foo
+
+try:
+    pass
+except:
+    pass
+finally:
+    pass
+
+
+try:
+    try:
+        pass
+    finally:
+        pass
+except foo:
+    pass
