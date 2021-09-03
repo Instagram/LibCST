cec6afee0225644892af1d50f56f6e8c610a48c5
Zsolt Dollenstein <zsol.zsol@gmail.com>
2021-09-03 16:47:51 +0100

    add tricky comments cases

--- native/libcst/tests/fixtures/comments.py
+++ native/libcst/tests/fixtures/comments.py
@@ -15,10 +15,10 @@ import sys
 import a
 from b.c.d.e import X  # some noqa comment
 
-# try:
-#     import fast
-# except ImportError:
-#     import slow as fast
+try:
+    import fast
+except ImportError:
+    import slow as fast
 
 
 # Some comment before a function.
@@ -41,56 +41,60 @@ def function(default=None):
         # Explains why we have this if.
         # In great detail indeed.
         x = X()
-    #     return x.method1()  # type: ignore
+        return x.method1()  # type: ignore
 
 
-#     # This return is also commented for some reason.
-#     return default
+    # This return is also commented for some reason.
+    return default
 
 
 # Explains why we use global state.
-# GLOBAL_STATE = {"a": a(1), "b": a(2), "c": a(3)}
+GLOBAL_STATE = {"a": a(1), "b": a(2), "c": a(3)}
 
 
 # Another comment!
 # This time two lines.
 
 
-# class Foo:
-#     """Docstring for class Foo.  Example from Sphinx docs."""
+class Foo:
+    """Docstring for class Foo.  Example from Sphinx docs."""
 
-#     #: Doc comment for class attribute Foo.bar.
-#     #: It can have multiple lines.
-#     bar = 1
+    #: Doc comment for class attribute Foo.bar.
+    #: It can have multiple lines.
+    bar = 1
 
-#     flox = 1.5  #: Doc comment for Foo.flox. One line only.
+    flox = 1.5  #: Doc comment for Foo.flox. One line only.
 
-#     baz = 2
-#     """Docstring for class attribute Foo.baz."""
+    baz = 2
+    """Docstring for class attribute Foo.baz."""
 
-#     def __init__(self):
-#         #: Doc comment for instance attribute qux.
-#         self.qux = 3
+    def __init__(self):
+        #: Doc comment for instance attribute qux.
+        self.qux = 3
 
-#         self.spam = 4
-#         """Docstring for instance attribute spam."""
+        self.spam = 4
+        """Docstring for instance attribute spam."""
 
 
 #' <h1>This is pweave!</h1>
 
 
-# @fast(really=True)
-# async def wat():
-#     # This comment, for some reason \
-#     # contains a trailing backslash.
-#     async with X.open_async() as x:  # Some more comments
-#         result = await x.method1()
-#     # Comment after ending a block.
-#     if result:
-#         print("A OK", file=sys.stdout)
-#         # Comment between things.
-#         print()
+@fast(really=True)
+async def wat():
+    # This comment, for some reason \
+    # contains a trailing backslash.
+    async with X.open_async() as x:  # Some more comments
+        result = await x.method1()
+    # Comment after ending a block.
+    if result:
+        print("A OK", file=sys.stdout)
+        # Comment between things.
+        print()
 
+
+if True:       # Hanging comments
+               # because why not
+    pass
 
 # Some closing comments.
 # Maybe Vim or Emacs directives for formatting.
