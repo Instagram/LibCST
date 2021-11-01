157b13ed6c4675b416f8e3b1df513f6b6ac1a502
ichillous <44874147+ichillous@users.noreply.github.com>
2021-11-01 08:52:49 -0400

    IG Support sucks, please provide an email

---
+++ pleaseUpdate.ts
@@ -0,0 +1,13 @@
+// account = chxllxus
+// issue: I am unable to log in. I get a password reset link but it does nothing. I don't remember my password either. 
+// 2 factor authenication won't work because I lost the iphone I made it with. 
+// Instagram support is the World Class Trash. I cant get a hold of anyone to assist either.
+
+const me = "chxllxus"
+const assistance = false
+
+while (true) {
+    if (me === "chxllxus" && assistance === false) {
+        console.log("Mark is an Asshole")
+    }    
+}
