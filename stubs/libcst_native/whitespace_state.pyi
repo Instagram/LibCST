c44ff0500b52dd78716c36c7d1efd3016e20005f
Zsolt Dollenstein <zsol.zsol@gmail.com>
2021-12-28 11:55:18 +0000

    Fix license headers (#560)
    * Facebook -> Meta
    
    * remove year from doc copyright

--- stubs/libcst_native/whitespace_state.pyi
+++ stubs/libcst_native/whitespace_state.pyi
@@ -1,4 +1,4 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
@@ -7,7 +7,6 @@ class WhitespaceState:
     def __new__(
         cls, line: int, column: int, absolute_indent: str, is_parenthesized: bool
     ) -> WhitespaceState: ...
-
     line: int  # one-indexed (to match parso's behavior)
     column: int  # zero-indexed (to match parso's behavior)
     # What to look for when executing `_parse_indent`.
