d2386a60d806214e0e64255d2773baf750428e2e
Zsolt Dollenstein <zsol.zsol@gmail.com>
2021-12-29 11:44:54 +0000

    add roundtrip tests

---
+++ native/libcst/tests/fixtures/starry_tries.py
@@ -0,0 +1,64 @@
+#foo.
+
+try   :
+    pass
+
+    # foo
+
+except *  lol  as  LOL  :
+
+    pass
+
+except  * f:
+
+    # foo
+
+    pass
+
+else  :
+
+    pass
+
+finally  :
+
+    foo
+
+try:
+    pass
+except*f:
+    pass
+finally:
+    pass
+
+
+try:
+
+    # 1
+
+    try:
+
+        #  2
+
+        pass
+
+        # 3
+
+    # 4
+
+    finally:
+
+        # 5
+
+        pass
+    
+        # 6
+    
+    # 7
+
+except *foo:
+
+    #8
+
+    pass
+
+    #9
