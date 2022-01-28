78468b50c9d68a923eee4e844ca827714b052b40
Zsolt Dollenstein <zsol.zsol@gmail.com>
2022-01-28 14:17:36 +0000

    fix copyright headers and add a script to check

--- native/roundtrip.sh
+++ native/roundtrip.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 PARSE=$(dirname $0)/target/release/parse
 
 exec diff -u "$1" <($PARSE < "$1")
