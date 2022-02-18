73e02f1c76389294f47cbee9543ce4f5dac4f3c0
Steven Troxler <steven.troxler@gmail.com>
2022-02-18 12:09:12 -0800

    Tweak the license format again.

--- libcst/codemod/visitors/_apply_type_annotations.py
+++ libcst/codemod/visitors/_apply_type_annotations.py
@@ -1,7 +1,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 #
 # This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree
+# LICENSE file in the root directory of this source tree.
 
 from dataclasses import dataclass
 from typing import Dict, List, Optional, Sequence, Set, Tuple, Union
