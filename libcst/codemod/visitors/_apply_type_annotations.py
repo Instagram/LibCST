8567fff2aa92046558c6bb74e9c3a9252ad99352
Steven Troxler <steven.troxler@gmail.com>
2022-02-18 10:50:48 -0800

    Remove trailing comment line from LICENSE header

--- libcst/codemod/visitors/_apply_type_annotations.py
+++ libcst/codemod/visitors/_apply_type_annotations.py
@@ -2,7 +2,6 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree
-#
 
 from dataclasses import dataclass
 from typing import Dict, List, Optional, Sequence, Set, Tuple, Union
