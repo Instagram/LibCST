87461db42843141e8e3cf32c4fdb8cb87b60cf19
Zsolt Dollenstein <zsol.zsol@gmail.com>
2023-08-28 21:46:42 +0100

    PEP 695 - Type Parameter Syntax
    This PR adds support for parsing and representing Type Parameters and Type Aliases as specified by PEP 695. What's missing are the scope rules, to be implemented in a future PR.
    
    Notable (user visible) changes:
    
    - new `TypeAlias` CST node, which is a `SmallStatement`
    - new CST nodes to represent TypeVarLikes: `TypeVar`, `TypeVarTuple`, `ParamSpec`
    - new helper CST nodes:  `TypeParameters` to serve as a container for multiple TypeVarLikes, and `TypeParam` which is a single item in a `TypeParameters` (owning the separating comma)
    - extended `FunctionDef` and `ClassDef` with an optional `type_parameters` field, as well as `whitespace_after_type_parameters` to own the extra whitespace between type parameters and the following token
      - these new fields are added after all others to avoid breaking callers passing in fields as positional arguments
    - in `FunctionDef` and `ClassDef`, `whitespace_after_name` now owns the whitespace before the type parameters if they exist

---
+++ native/libcst/tests/fixtures/type_parameters.py
@@ -0,0 +1,57 @@
+# fmt: off
+
+type TA = int
+
+type TA1[A] = lambda A: A
+
+class Outer[A]:
+    type TA1[A] = None
+
+type TA1[A, B] = dict[A, B]
+
+class Outer[A]:
+    def inner[B](self):
+        type TA1[C] = TA1[A, B] | int
+        return TA1
+
+def more_generic[T, *Ts, **P]():
+    type TA[T2, *Ts2, **P2] = tuple[Callable[P, tuple[T, *Ts]], Callable[P2, tuple[T2, *Ts2]]]
+    return TA
+
+type Recursive = Recursive
+
+def func[A](A): return A
+
+class ClassA:
+    def func[__A](self, __A): return __A
+
+class ClassA[A, B](dict[A, B]):
+    ...
+
+class ClassA[A]:
+    def funcB[B](self):
+        class ClassC[C]:
+            def funcD[D](self):
+                return lambda: (A, B, C, D)
+        return ClassC
+
+class Child[T](Base[lambda: (int, outer_var, T)]): ...
+
+type Alias[T: ([T for T in (T, [1])[1]], T)] = [T for T in T.__name__]
+type Alias[T: [lambda: T for T in (T, [1])[1]]] = [lambda: T for T in T.__name__]
+
+class Foo[T: Foo, U: (Foo, Foo)]:
+    pass
+
+def func[T](a: T = "a", *, b: T = "b"):
+    return (a, b)
+
+def func1[A: str, B: str | int, C: (int, str)]():
+    return (A, B, C)
+
+type   A [  T ,  *  V ]  =foo;type B=A
+
+def AAAAAAAAAAAAAAAAAA [  T :  int  ,*Ts  ,  ** TT  ]  ():pass
+class AAAAAAAAAAAAAAAAAA [  T :  int  ,*Ts  ,  ** TT  ] :pass
+
+def yikes[A:int,*B,**C](*d:*tuple[A,*B,...])->A:pass
