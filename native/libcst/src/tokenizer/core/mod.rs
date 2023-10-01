9d3dd49a94bc95ee9c67438d720b1b55de8b8436
Zsolt Dollenstein <zsol@fb.com>
2023-10-01 20:26:38 +0100

    allow multiline expressions in f-strings

--- native/libcst/src/tokenizer/core/mod.rs
+++ native/libcst/src/tokenizer/core/mod.rs
@@ -363,7 +363,8 @@ impl<'t> TokState<'t> {
                     self.text_pos.next();
                     self.at_bol = true;
                     if self.split_fstring
-                        && !self.fstring_stack.iter().all(|node| node.allow_multiline())
+                        && self.fstring_stack.last().map(|node| node.allow_multiline())
+                            == Some(false)
                     {
                         Err(TokError::UnterminatedString)
                     } else if self.blank_line || !self.paren_stack.is_empty() {
@@ -895,7 +896,8 @@ impl<'t> TokState<'t> {
         is_in_format_spec: bool,
         is_raw_string: bool,
     ) -> Result<Option<TokType>, TokError<'t>> {
-        let allow_multiline = self.fstring_stack.iter().all(|node| node.allow_multiline());
+        let allow_multiline =
+            self.fstring_stack.last().map(|node| node.allow_multiline()) == Some(true);
         let mut in_named_unicode: bool = false;
         let mut ok_result = Ok(None); // value to return if we reach the end and don't error out
         'outer: loop {
--- native/libcst/tests/fixtures/super_strings.py
+++ native/libcst/tests/fixtures/super_strings.py
@@ -27,6 +27,16 @@ _(f"ok { expr = !r: aosidjhoi } end")
 
 print(f"{self.ERASE_CURRENT_LINE}{self._human_seconds(elapsed_time)} {percent:.{self.pretty_precision}f}% complete, {self.estimate_completion(elapsed_time, finished, left)} estimated for {left} files to go...")
 
+f"{"\n".join()}"
+
+f"___{
+    x
+}___"
+
+f"___{(
+    x
+)}___"
+
 f'\{{\}}'
 f"regexp_like(path, '.*\{file_type}$')"
 f"\lfoo"
