445c6122825f3eb48202cf128384849f6ba60a4c
Kyle Into <kylei@fb.com>
2023-10-05 16:43:05 -0700

    remove extra import

--- libcst/metadata/file_path_provider.py
+++ libcst/metadata/file_path_provider.py
@@ -4,7 +4,7 @@
 # LICENSE file in the root directory of this source tree.
 
 from pathlib import Path
-from typing import Collection, List, Mapping, Optional
+from typing import List, Mapping, Optional
 
 import libcst as cst
 from libcst.metadata.base_provider import BatchableMetadataProvider
