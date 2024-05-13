efc53af60885853bfbd87564fa01ac25fcd2b94e
zaicruvoir1rominet <105832258+zaicruvoir1rominet@users.noreply.github.com>
2024-05-13 11:20:47 +0200

    Add helper functions for common ways of filtering nodes (#1137)
    * Make the nodes fields filtering process - from libcst.tool - public, so that other libraries may provide their own custom representation of LibCST graphs.
    
    * Create functions to access & filter CST-node fields (with appropriate docstrings & tests), in libcst.helpers
    
    * Add new CST-node fields functions to helpers documentation.

--- docs/source/helpers.rst
+++ docs/source/helpers.rst
@@ -32,3 +32,18 @@ Functions that assist in traversing an existing LibCST tree.
 .. autofunction:: libcst.helpers.get_full_name_for_node
 .. autofunction:: libcst.helpers.get_full_name_for_node_or_raise
 .. autofunction:: libcst.helpers.ensure_type
+
+Node fields filtering Helpers
+-----------------------------
+
+Function that assist when handling CST nodes' fields.
+
+.. autofunction:: libcst.helpers.filter_node_fields
+
+And lower level functions:
+
+.. autofunction:: libcst.helpers.get_node_fields
+.. autofunction:: libcst.helpers.is_whitespace_node_field
+.. autofunction:: libcst.helpers.is_syntax_node_field
+.. autofunction:: libcst.helpers.is_default_node_field
+.. autofunction:: libcst.helpers.get_field_default_value
--- libcst/tool.py
+++ libcst/tool.py
@@ -22,15 +22,7 @@ from typing import Any, Callable, Dict, List, Sequence, Tuple, Type
 
 import yaml
 
-from libcst import (
-    CSTNode,
-    IndentedBlock,
-    LIBCST_VERSION,
-    Module,
-    parse_module,
-    PartialParserConfig,
-)
-from libcst._nodes.deep_equals import deep_equals
+from libcst import CSTNode, LIBCST_VERSION, parse_module, PartialParserConfig
 from libcst._parser.parso.utils import parse_version_string
 from libcst.codemod import (
     CodemodCommand,
@@ -40,6 +32,7 @@ from libcst.codemod import (
     gather_files,
     parallel_exec_transform_with_prettyprint,
 )
+from libcst.helpers import filter_node_fields
 
 _DEFAULT_INDENT: str = "  "
 
@@ -54,76 +47,14 @@ def _node_repr_recursive(  # noqa: C901
 ) -> List[str]:
     if isinstance(node, CSTNode):
         # This is a CSTNode, we must pretty-print it.
+        fields: Sequence["dataclasses.Field[CSTNode]"] = filter_node_fields(
+            node=node,
+            show_defaults=show_defaults,
+            show_syntax=show_syntax,
+            show_whitespace=show_whitespace,
+        )
+
         tokens: List[str] = [node.__class__.__name__]
-        fields: Sequence["dataclasses.Field[object]"] = dataclasses.fields(node)
-
-        # Hide all fields prefixed with "_"
-        fields = [f for f in fields if f.name[0] != "_"]
-
-        # Filter whitespace nodes if needed
-        if not show_whitespace:
-
-            def _is_whitespace(field: "dataclasses.Field[object]") -> bool:
-                if "whitespace" in field.name:
-                    return True
-                if "leading_lines" in field.name:
-                    return True
-                if "lines_after_decorators" in field.name:
-                    return True
-                if isinstance(node, (IndentedBlock, Module)) and field.name in [
-                    "header",
-                    "footer",
-                ]:
-                    return True
-                if isinstance(node, IndentedBlock) and field.name == "indent":
-                    return True
-                return False
-
-            fields = [f for f in fields if not _is_whitespace(f)]
-        # Filter values which aren't changed from their defaults
-        if not show_defaults:
-
-            def _get_default(fld: "dataclasses.Field[object]") -> object:
-                if fld.default_factory is not dataclasses.MISSING:
-                    # pyre-fixme[29]: `Union[dataclasses._MISSING_TYPE,
-                    #  dataclasses._DefaultFactory[object]]` is not a function.
-                    return fld.default_factory()
-                return fld.default
-
-            fields = [
-                f
-                for f in fields
-                if not deep_equals(getattr(node, f.name), _get_default(f))
-            ]
-        # Filter out values which aren't interesting if needed
-        if not show_syntax:
-
-            def _is_syntax(field: "dataclasses.Field[object]") -> bool:
-                if isinstance(node, Module) and field.name in [
-                    "encoding",
-                    "default_indent",
-                    "default_newline",
-                    "has_trailing_newline",
-                ]:
-                    return True
-                type_str = repr(field.type)
-                if (
-                    "Sentinel" in type_str
-                    and field.name not in ["star_arg", "star", "posonly_ind"]
-                    and "whitespace" not in field.name
-                ):
-                    # This is a value that can optionally be specified, so its
-                    # definitely syntax.
-                    return True
-
-                for name in ["Semicolon", "Colon", "Comma", "Dot", "AssignEqual"]:
-                    # These are all nodes that exist for separation syntax
-                    if name in type_str:
-                        return True
-
-                return False
-
-            fields = [f for f in fields if not _is_syntax(f)]
 
         if len(fields) == 0:
             tokens.append("()")
@@ -204,12 +135,12 @@ def dump(
     from the default contruction of the node while also hiding whitespace and
     syntax fields.
 
-    Setting ``show_default`` to ``True`` will add fields regardless if their
+    Setting ``show_defaults`` to ``True`` will add fields regardless if their
     value is different from the default value.
 
     Setting ``show_whitespace`` will add whitespace fields and setting
     ``show_syntax`` will add syntax fields while respecting the value of
-    ``show_default``.
+    ``show_defaults``.
 
     When all keyword args are set to true, the output of this function is
     indentical to the __repr__ method of the node.
