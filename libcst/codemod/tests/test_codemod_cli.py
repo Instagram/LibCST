45234f198c22aed1227c347d2fcc29c2f003d386
Kirill Ignatev <kirill.ign@gmail.com>
2024-08-05 17:41:51 -0400

    Clear warnings for each file in comemod cli (#1184)
    * Clean warnings for each file in comemod cli
    
    * Fix ZeroDivisionError: float division by zero
    
    When codemodding too fast
    
    * Recreate CodemodContext for each file
    
    Keep only context.metadata_manager
    Remove wrapper from context defaults on each file

--- libcst/codemod/tests/test_codemod_cli.py
+++ libcst/codemod/tests/test_codemod_cli.py
@@ -8,10 +8,12 @@
 import platform
 import subprocess
 import sys
+import tempfile
 from pathlib import Path
 from unittest import skipIf
 
 from libcst._parser.entrypoints import is_native
+from libcst.codemod import CodemodTest
 from libcst.testing.utils import UnitTest
 
 
@@ -63,3 +65,31 @@ class TestCodemodCLI(UnitTest):
             stderr=subprocess.STDOUT,
         )
         assert "Finished codemodding 1 files!" in output
+
+    def test_warning_messages_several_files(self) -> None:
+        code = """
+        def baz() -> str:
+            return "{}: {}".format(*baz)
+        """
+        with tempfile.TemporaryDirectory() as tmpdir:
+            p = Path(tmpdir)
+            (p / "mod1.py").write_text(CodemodTest.make_fixture_data(code))
+            (p / "mod2.py").write_text(CodemodTest.make_fixture_data(code))
+            (p / "mod3.py").write_text(CodemodTest.make_fixture_data(code))
+            output = subprocess.run(
+                [
+                    sys.executable,
+                    "-m",
+                    "libcst.tool",
+                    "codemod",
+                    "convert_format_to_fstring.ConvertFormatStringCommand",
+                    str(p),
+                ],
+                encoding="utf-8",
+                stderr=subprocess.PIPE,
+            )
+            # Each module will generate a warning, so we should get 3 warnings in total
+            self.assertIn(
+                "- 3 warnings were generated.",
+                output.stderr,
+            )
