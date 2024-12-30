ae77ed40a61ca2418259701765ac52e0c42f3089
yangdanny97 <dzy4@cornell.edu>
2024-12-29 20:18:18 -0500

    add codemod to rename typing aliases of builtins

---
+++ libcst/codemod/commands/rename_typing_generic_aliases.py
@@ -0,0 +1,31 @@
+from functools import partial
+from typing import cast, Generator
+
+from libcst.codemod import Codemod, MagicArgsCodemodCommand
+from libcst.codemod.commands.rename import RenameCommand
+
+
+class RenameTypingGenericAliases(MagicArgsCodemodCommand):
+    DESCRIPTION: str = (
+        "Rename typing module aliases of builtin generics in Python 3.9+, for example: `typing.List` -> `list`"
+    )
+
+    MAPPING: dict[str, str] = {
+        "typing.List": "builtins.list",
+        "typing.Tuple": "builtins.tuple",
+        "typing.Dict": "builtins.dict",
+        "typing.FrozenSet": "builtins.frozenset",
+        "typing.Set": "builtins.set",
+        "typing.Type": "builtins.type",
+    }
+
+    def get_transforms(self) -> Generator[type[Codemod], None, None]:
+        for from_type, to_type in self.MAPPING.items():
+            yield cast(
+                type[Codemod],
+                partial(
+                    RenameCommand,
+                    old_name=from_type,
+                    new_name=to_type,
+                ),
+            )
