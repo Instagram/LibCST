9bfac550ec87e3b169fc5377a5ab46b4688a5253
yangdanny97 <dzy4@cornell.edu>
2025-01-02 17:56:07 -0500

    early return

--- libcst/codemod/commands/convert_union_to_or.py
+++ libcst/codemod/commands/convert_union_to_or.py
@@ -23,29 +23,29 @@ class ConvertUnionToOrCommand(VisitorBasedCodemodCommand):
         Given a subscript, check if it's a Union - if so, either flatten the members
         into a nested BitOr (if multiple members) or unwrap the type (if only one member).
         """
-        if QualifiedNameProvider.has_name(
+        if not QualifiedNameProvider.has_name(
             self,
             original_node,
             QualifiedName(name="typing.Union", source=QualifiedNameSource.IMPORT),
         ):
-            types = [
-                cst.ensure_type(
-                    cst.ensure_type(s, cst.SubscriptElement).slice, cst.Index
-                ).value
-                for s in updated_node.slice
-            ]
-            if len(types) == 1:
-                replacement = types[0]
-            else:
+            return updated_node
+        types = [
+            cst.ensure_type(
+                cst.ensure_type(s, cst.SubscriptElement).slice, cst.Index
+            ).value
+            for s in updated_node.slice
+        ]
+        if len(types) == 1:
+            return types[0]
+        else:
+            replacement = cst.BinaryOperation(
+                left=types[0], right=types[1], operator=cst.BitOr()
+            )
+            for type_ in types[2:]:
                 replacement = cst.BinaryOperation(
-                    left=types[0], right=types[1], operator=cst.BitOr()
+                    left=replacement, right=type_, operator=cst.BitOr()
                 )
-                for type_ in types[2:]:
-                    replacement = cst.BinaryOperation(
-                        left=replacement, right=type_, operator=cst.BitOr()
-                    )
             return replacement
-        return updated_node
 
     def leave_Module(
         self, original_node: cst.Module, updated_node: cst.Module
